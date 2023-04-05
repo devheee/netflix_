a73582aeee0a277c4a73c75b6fcbfe52a9c5f5f7
SOHEE <soheee94@gmail.com>
2023-04-05 14:17:10 +0900

    0405

--- src/Home.js
+++ src/Home.js
@@ -130,20 +130,22 @@ const Home = () => {
                     </section>
                 </main>
                 <footer className='footer'>
-                    <dl>
-                        <p>넷플릭스서비시스코리아 유한회사 / 통신판매업신고번호: 제2018-서울종로-0426호 / 전화번호: 080-001-9587</p>
-                        <dt>대표</dt>
-                        <dd>레지널드 숀 톰프슨</dd>
-                        <dt>이메일 주소</dt>
-                        <dd>korea@netflix.com</dd>
-                        <dt>주소</dt>
-                        <dd>대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161</dd>
-                        <dt>사업자등록번호</dt>
-                        <dd>165-87-00119</dd>
-                        <dt>클라우드 호스팅</dt>
-                        <dd>Amazon Web Services Inc.</dd>
-                        <a href="#">공정거래위원회 웹사이트</a>
-                    </dl>
+                    <div className="h_inner">
+                        <dl>
+                            <p>넷플릭스서비시스코리아 유한회사 / 통신판매업신고번호: 제2018-서울종로-0426호 / 전화번호: 080-001-9587</p>
+                            <dt>대표</dt>
+                            <dd>레지널드 숀 톰프슨</dd>
+                            <dt>이메일 주소</dt>
+                            <dd>korea@netflix.com</dd>
+                            <dt>주소</dt>
+                            <dd>대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161</dd>
+                            <dt>사업자등록번호</dt>
+                            <dd>165-87-00119</dd>
+                            <dt>클라우드 호스팅</dt>
+                            <dd>Amazon Web Services Inc.</dd>
+                            <a href="#">공정거래위원회 웹사이트</a>
+                        </dl>
+                    </div>
                 </footer>
 
             </div>
